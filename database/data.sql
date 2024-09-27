a1bb0e2d97d01836f53d1720717d7def136bcaf7
root <mrluigiclemente@g.com>
2024-09-27 04:55:33 +0000

    data updated

--- database/data.sql
+++ database/data.sql
@@ -67,6 +67,81 @@ VALUES
 ('French Toast', '{"Calories":400,"Carbs":50,"Protein":10}', 1, 1, 'American'),  -- Breakfast category, Meal Type: Breakfast
 ('Chicken Stir-fry', '{"Calories":450,"Carbs":30,"Protein":30}', 18, 3, 'Asian'); -- Stir-fry category, Meal Type: Dinner
 
+-- Insert additional ingredients into the ingredients table
+INSERT INTO ingredients (meal_id, name, amount, portion)
+VALUES 
+-- For Pancakes (Meal ID: 1)
+(1, 'Flour', 100.0, '1 cup'), 
+(1, 'Milk', 200.0, '1 cup'),
+(1, 'Eggs', 2.0, '2 large'),
+(1, 'Maple Syrup', 50.0, '2 tbsp'),
+
+-- For Caesar Salad (Meal ID: 2)
+(2, 'Croutons', 50.0, '1/2 cup'), 
+(2, 'Parmesan Cheese', 30.0, '1/4 cup'),
+(2, 'Caesar Dressing', 50.0, '3 tbsp'),
+(2, 'Grilled Chicken', 100.0, '1 serving'),
+
+-- For Spaghetti Bolognese (Meal ID: 3)
+(3, 'Spaghetti', 150.0, '1 serving'),
+(3, 'Ground Beef', 200.0, '1 cup'),
+(3, 'Tomato Sauce', 100.0, '1/2 cup'),
+(3, 'Onion', 50.0, '1 small'),
+
+-- For Margherita Pizza (Meal ID: 4)
+(4, 'Pizza Dough', 200.0, '1 serving'),
+(4, 'Tomato Sauce', 100.0, '1/2 cup'),
+(4, 'Mozzarella Cheese', 150.0, '1 cup'),
+(4, 'Basil', 10.0, 'Handful'),
+
+-- For Veggie Burger (Meal ID: 5)
+(5, 'Veggie Patty', 150.0, '1 patty'),
+(5, 'Whole Wheat Bun', 1.0, '1 bun'),
+(5, 'Lettuce', 30.0, '1 leaf'),
+(5, 'Tomato', 50.0, '1 slice'),
+
+-- For Chicken Tacos (Meal ID: 6)
+(6, 'Chicken Breast', 150.0, '1 serving'),
+(6, 'Taco Shells', 3.0, '3 shells'),
+(6, 'Cheese', 50.0, '1/4 cup'),
+(6, 'Salsa', 50.0, '2 tbsp'),
+
+-- For Chocolate Cake (Meal ID: 7)
+(7, 'Flour', 100.0, '1 cup'),
+(7, 'Sugar', 150.0, '3/4 cup'),
+(7, 'Cocoa Powder', 50.0, '1/4 cup'),
+(7, 'Butter', 100.0, '1/2 cup'),
+
+-- For Grilled Salmon (Meal ID: 8)
+(8, 'Salmon Fillet', 200.0, '1 serving'),
+(8, 'Olive Oil', 15.0, '1 tbsp'),
+(8, 'Lemon', 1.0, '1 wedge'),
+(8, 'Herbs', 5.0, '1 tsp'),
+
+-- For Fruit Salad (Meal ID: 9)
+(9, 'Apple', 100.0, '1 medium'),
+(9, 'Banana', 120.0, '1 medium'),
+(9, 'Grapes', 50.0, '1/2 cup'),
+(9, 'Orange', 100.0, '1 medium'),
+
+-- For Quinoa Bowl (Meal ID: 10)
+(10, 'Quinoa', 100.0, '1/2 cup'),
+(10, 'Cucumber', 50.0, '1/2 cup'),
+(10, 'Avocado', 50.0, '1/2 avocado'),
+(10, 'Olive Oil', 10.0, '1 tbsp'),
+
+-- For French Toast (Meal ID: 11)
+(11, 'Bread', 100.0, '2 slices'),
+(11, 'Eggs', 2.0, '2 large'),
+(11, 'Milk', 100.0, '1/2 cup'),
+(11, 'Butter', 20.0, '1 tbsp'),
+
+-- For Chicken Stir-fry (Meal ID: 12)
+(12, 'Chicken Breast', 150.0, '1 serving'),
+(12, 'Bell Peppers', 100.0, '1 cup'),
+(12, 'Soy Sauce', 30.0, '2 tbsp'),
+(12, 'Garlic', 10.0, '1 clove');
+
 
 -- Insert more meals into the meals table
 INSERT INTO meals (name, nutritional_content, meal_category_id, meal_type_id, cuisine)
@@ -87,14 +162,104 @@ VALUES
 ('Lentil Soup', '{"Calories":250,"Carbs":40,"Protein":18}', 3, 2, 'Mediterranean'); -- Soup category
 
 
-
+-- Insert ingredients for new meals
 INSERT INTO ingredients (meal_id, name, amount, portion)
 VALUES 
-(1, 'Oats', 50.0, '1/2 cup'), 
-(1, 'Berries', 100.0, '1 cup'),
-(2, 'Lettuce', 150.0, '1 bowl'), 
-(2, 'Tomatoes', 50.0, '1 medium'),
-(3, 'Chicken Breast', 200.0, '1 serving');
+-- For Greek Yogurt Parfait (Meal ID: 13)
+(13, 'Greek Yogurt', 200.0, '1 cup'),
+(13, 'Granola', 50.0, '1/2 cup'),
+(13, 'Honey', 20.0, '1 tbsp'),
+(13, 'Mixed Berries', 100.0, '1/2 cup'),
+
+-- For Caprese Salad (Meal ID: 14)
+(14, 'Tomatoes', 100.0, '2 medium'),
+(14, 'Mozzarella Cheese', 125.0, '1 ball'),
+(14, 'Basil', 10.0, 'Handful'),
+(14, 'Olive Oil', 15.0, '1 tbsp'),
+(14, 'Balsamic Vinegar', 10.0, '1 tbsp'),
+
+-- For Chicken Alfredo (Meal ID: 15)
+(15, 'Fettuccine Pasta', 150.0, '1 serving'),
+(15, 'Chicken Breast', 200.0, '1 serving'),
+(15, 'Heavy Cream', 100.0, '1/2 cup'),
+(15, 'Parmesan Cheese', 50.0, '1/4 cup'),
+(15, 'Garlic', 10.0, '2 cloves'),
+
+-- For BBQ Chicken Pizza (Meal ID: 16)
+(16, 'Pizza Dough', 200.0, '1 serving'),
+(16, 'BBQ Sauce', 50.0, '1/4 cup'),
+(16, 'Chicken Breast', 150.0, '1 serving'),
+(16, 'Mozzarella Cheese', 100.0, '1 cup'),
+(16, 'Red Onion', 50.0, '1/2 medium'),
+
+-- For Beef Tacos (Meal ID: 17)
+(17, 'Ground Beef', 150.0, '1 serving'),
+(17, 'Taco Shells', 3.0, '3 shells'),
+(17, 'Cheddar Cheese', 50.0, '1/4 cup'),
+(17, 'Salsa', 30.0, '2 tbsp'),
+(17, 'Lettuce', 30.0, '1 leaf'),
+
+-- For Mango Sticky Rice (Meal ID: 18)
+(18, 'Sticky Rice', 150.0, '1/2 cup'),
+(18, 'Mango', 200.0, '1 medium'),
+(18, 'Coconut Milk', 100.0, '1/4 cup'),
+(18, 'Sugar', 50.0, '2 tbsp'),
+
+-- For Shrimp Fried Rice (Meal ID: 19)
+(19, 'Rice', 150.0, '1 cup'),
+(19, 'Shrimp', 150.0, '1 serving'),
+(19, 'Soy Sauce', 30.0, '2 tbsp'),
+(19, 'Eggs', 2.0, '2 large'),
+(19, 'Vegetables', 100.0, '1 cup mixed'),
+
+-- For Veggie Stir-fry (Meal ID: 20)
+(20, 'Bell Peppers', 100.0, '1 cup'),
+(20, 'Broccoli', 100.0, '1 cup'),
+(20, 'Carrots', 50.0, '1/2 cup'),
+(20, 'Soy Sauce', 30.0, '2 tbsp'),
+(20, 'Garlic', 10.0, '1 clove'),
+
+-- For Peanut Butter Banana Smoothie (Meal ID: 21)
+(21, 'Banana', 120.0, '1 medium'),
+(21, 'Peanut Butter', 30.0, '2 tbsp'),
+(21, 'Greek Yogurt', 100.0, '1/2 cup'),
+(21, 'Milk', 200.0, '1 cup'),
+(21, 'Honey', 15.0, '1 tbsp'),
+
+-- For Roasted Vegetable Quinoa (Meal ID: 22)
+(22, 'Quinoa', 100.0, '1/2 cup'),
+(22, 'Zucchini', 50.0, '1/2 cup'),
+(22, 'Bell Peppers', 50.0, '1/2 cup'),
+(22, 'Olive Oil', 15.0, '1 tbsp'),
+(22, 'Lemon Juice', 10.0, '1 tbsp'),
+
+-- For Omelette (Meal ID: 23)
+(23, 'Eggs', 3.0, '3 large'),
+(23, 'Cheddar Cheese', 50.0, '1/4 cup'),
+(23, 'Bell Peppers', 50.0, '1/4 cup'),
+(23, 'Onion', 30.0, '1/4 medium'),
+(23, 'Butter', 10.0, '1 tbsp'),
+
+-- For Chocolate Chip Cookies (Meal ID: 24)
+(24, 'Flour', 120.0, '1 cup'),
+(24, 'Sugar', 100.0, '1/2 cup'),
+(24, 'Butter', 100.0, '1/2 cup'),
+(24, 'Chocolate Chips', 100.0, '1/2 cup'),
+(24, 'Egg', 1.0, '1 large'),
+
+-- For Beef Stir-fry (Meal ID: 25)
+(25, 'Beef', 200.0, '1 serving'),
+(25, 'Bell Peppers', 100.0, '1 cup'),
+(25, 'Soy Sauce', 30.0, '2 tbsp'),
+(25, 'Broccoli', 100.0, '1 cup'),
+(25, 'Garlic', 10.0, '1 clove'),
+
+-- For Lentil Soup (Meal ID: 26)
+(26, 'Lentils', 150.0, '1 cup'),
+(26, 'Carrots', 100.0, '1 cup'),
+(26, 'Celery', 50.0, '1/2 cup'),
+(26, 'Onion', 50.0, '1 medium'),
+(26, 'Garlic', 10.0, '2 cloves');
 
 
 
